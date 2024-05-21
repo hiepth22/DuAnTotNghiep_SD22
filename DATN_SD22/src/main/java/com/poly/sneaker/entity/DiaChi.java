b207259a31bfd1fbfda4fa1e362a7ff5b0fbf529
hiepth22 <106100259+hiepth22@users.noreply.github.com>
2024-05-21 23:38:29 +0700

    Create file

---
+++ DATN_SD22/src/main/java/com/poly/sneaker/entity/DiaChi.java
@@ -0,0 +1,22 @@
+package com.poly.sneaker.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+
+public class DiaChi {
+}
