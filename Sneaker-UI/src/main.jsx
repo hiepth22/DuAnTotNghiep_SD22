8c2252d209c6582d4e3326043f011f48a6dd4ed0
hiepth22 <106100259+hiepth22@users.noreply.github.com>
2024-05-27 00:58:13 +0700

    Config json

--- Sneaker-UI/src/main.jsx
+++ Sneaker-UI/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import "../node_modules/@fortawesome/fontawesome-free/css/all.min.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/ReactToastify.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+);
